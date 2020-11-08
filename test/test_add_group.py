e8ea5e6ae5bb7cf730fe2828c2adcfd2ee867b3c
Seleznyov <58526725+Seleznyov@users.noreply.github.com>
2020-11-08 14:09:17 +0300

    Добавлена генерация случайных тестовых данных

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 from python_traning.model.group import Group
 import pytest
+import random
+import string
 
-testdata=[Group(name="1", header="1", footer="1"),Group(name="", header="", footer="")]
+def random_string(prefics, maxlen):
+    # Добавим string.punctuation (знаки пунктуации)
+    symbols=string.ascii_letters+string.digits+" "*10
+    return prefics +"".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+# Первый варианг герации данных (одна пустая строка, и остальные случайные наборы)
+# testdata=[Group(name="", header="", footer="")]+[
+#     Group(name=random_string("name",10), header=random_string("header",10), footer=random_string("footer",10))
+#     for i in range(5)
+# ]
+
+# Второй вариант полный перебор
+testdata=[Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name",10)]
+    for header in ["", random_string("header",10)]
+    for footer in ["", random_string("footer",10)]
+]
 
 @pytest.mark.parametrize("group",testdata, ids=[str(x) for x in testdata])
 def test_add_group(app,group):
